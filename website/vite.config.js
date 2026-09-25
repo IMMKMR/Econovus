8a4ad3637f66b51dc8bf6ec09df176820f3cfeee
Immanuel Kumar <immanuelkumar22@gmail.com>
2026-09-25 13:46:31 +0530

    chore: include additional html files in build

--- website/vite.config.js
+++ website/vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        exploded: 'exploded-view.html',
+        warehouse: 'warehouse.html'
+      }
+    }
+  }
 })
 
